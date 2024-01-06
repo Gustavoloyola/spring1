b3f7fb0a1f47cd4d3a454881531583ce744c91b9
Gustavoloyola <84485135+Gustavoloyola@users.noreply.github.com>
2024-01-05 19:32:48 -0600

    este es mi primer commit de spring

---
+++ src/test/java/com/example/ejemindata2/Ejemindata2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemindata2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemindata2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
