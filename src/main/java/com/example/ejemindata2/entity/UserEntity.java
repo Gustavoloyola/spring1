dd66f221d0ecff8eaedb4fe37eec235af1b506b4
Gustavoloyola <84485135+Gustavoloyola@users.noreply.github.com>
2024-01-05 22:34:25 -0600

    creacion entidad mascota relacionada de 1 a muchos

--- src/main/java/com/example/ejemindata2/entity/UserEntity.java
+++ src/main/java/com/example/ejemindata2/entity/UserEntity.java
@@ -1,26 +1,32 @@
 package com.example.ejemindata2.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
+@Table(name = "Usuario")
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "Nombre", nullable = true)
     private String name;
+
+    @Column(name = "Edad")
     private Integer age;
 
+    @OneToMany(mappedBy = "user")
+    private List<MascotaEntity> masc=new ArrayList<>();
 
 
 
